9f8ee882c62638531ccf6aa23d0f060915f12841
hado0123 <hado_123@naver.com>
2025-11-16 23:28:38 +0900

    first commit

---
+++ src/main/java/com/creatorhub/CreatorhubServerApplication.java
@@ -0,0 +1,11 @@
+package com.creatorhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorhubServerApplication {
+
+    public static void main(String[] args) { SpringApplication.run(CreatorhubServerApplication.class, args); }
+
+}
