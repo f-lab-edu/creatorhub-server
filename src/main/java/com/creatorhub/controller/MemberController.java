6c1d58f9a72ceab7a55a324d13838bc982fc9a47
hado0123 <hado_123@naver.com>
2025-11-23 23:54:34 +0900

    [#1] style: 로그 구현체 Slf4j로 변경

--- src/main/java/com/creatorhub/controller/MemberController.java
+++ src/main/java/com/creatorhub/controller/MemberController.java
@@ -1,18 +1,16 @@
 package com.creatorhub.controller;
 
-import com.creatorhub.dto.MemberRequestDto;
-import com.creatorhub.dto.MemberResponseDto;
+import com.creatorhub.dto.MemberRequest;
+import com.creatorhub.dto.MemberResponse;
 import com.creatorhub.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/members")
-@Log4j2
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -23,8 +21,8 @@ public class MemberController {
      * POST /api/members/signup
      */
     @PostMapping("/signup")
-    public ResponseEntity<MemberResponseDto> signup(@Valid @RequestBody MemberRequestDto memberRequestDto) {
-        MemberResponseDto memberResponseDto = memberService.signup(memberRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(memberResponseDto);
+    public ResponseEntity<MemberResponse> signup(@Valid @RequestBody MemberRequest memberRequest) {
+        MemberResponse memberResponse = memberService.signup(memberRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberResponse);
     }
 }
