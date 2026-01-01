0797cf013a77901b20897e3ccc0065911882ced4
hado0123 <hado_123@naver.com>
2026-01-01 18:48:47 +0900

    [#21] refactor: 미사용 주석 코드 제거

--- src/main/java/com/creatorhub/dto/CreatorResponse.java
+++ src/main/java/com/creatorhub/dto/CreatorResponse.java
@@ -1,23 +1,19 @@
 package com.creatorhub.dto;
 
 import com.creatorhub.entity.Creator;
-//import com.creatorhub.entity.FileObject;
 
 public record CreatorResponse(
         Long id,
         Long memberId,
         String creatorName,
         String introduction
-//        FileObjectResponse profileImage
 ) {
     public static CreatorResponse from(Creator creator) {
-//        FileObject fileObject = creator.getFileObject();
         return new CreatorResponse(
                 creator.getId(),
                 creator.getMember().getId(),
                 creator.getCreatorName(),
                 creator.getIntroduction()
-//                FileObjectResponse.from(fileObject)
         );
     }
 }
